8158814d8e5392f1a0013fc4438d6a7f16ee64da
exoks <exoks.team@gmail.com>
2023-12-16 19:48:39 +0100

    parser int progress

---
+++ main.cpp
@@ -0,0 +1,35 @@
+//            ################                                                  
+//          ####################                                                
+//        ########################                                              
+//       #############+########### #                                            
+//       ######-..        .+########        < main.cpp >                        
+//       ####-..            ..+####                                             
+//       ###-...             .-####                                             
+//       ###...              ..+##    Student: oussama <oezzaou@student.1337.ma>
+//        #-.++###.      -###+..##                                              
+//        #....  ...   .-.  ....##       Created: 2023/12/16 19:43:32 by oussama
+//     --.#.-#+## -..  -+ ##-#-.-...     Updated: 2023/12/16 19:43:32 by oussama
+//      ---....... ..  ........... -                                            
+//      -+#..     ..   .       .+-.                                             
+//       .--.     .     .     ..+.                                              
+//         -..    .+--.-.     ...                                               
+//         +.... .-+#.#+.    ..-                                                
+//          +...#####-++###-..-                                                 
+//          #---..----+--.---+##                                                
+//        ###-+--.... ....--+#####                                              
+//  ##########--#-.......-#-###########      Made By Oussama Ezzaou <OEZZAOU> :)
+
+# include "KeyValueParser.hpp"
+
+int	main(void)
+{
+//	prs::KeyValueParser<std::string, int> p("test");
+//	std::map<std::string, int> mp;
+//
+//	mp = p.parseFile();
+//	std::cout << "size : " << mp.size() << std::endl;
+//	for (std::map<std::string, int>::iterator i = mp.begin(); i != mp.end(); ++i)
+//		std::cout << "============" << std::endl;
+	psr::test();
+	return (EXIT_SUCCESS);
+}
